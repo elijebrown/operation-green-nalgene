d7015864934c451b6f83b0b68ce4e920268f7751
Elijah Bruhn <elijah@vectorface.com>
2024-12-10 17:58:05 -0500

    added paradeDB docker container for database

--- packages/back-express/src/server.js
+++ packages/back-express/src/server.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const app = express();
+const PORT = 3000
 
 // Define a simple route
 app.get('/', (req, res) => {
     res.send(`<h1>hello </h1>`);
 });
 
-
-
-// Start the server on port 8080
-const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
