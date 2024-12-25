e278a8182b77f87955cf62be48218db78c00a938
Elijah Bruhn <elijah.bruhn@gmail.com>
2024-12-26 00:04:03 +0100

    added correct ngram indexes in txt file. searches configured properly and workinggit statusgit status
    '
    '

--- oldDBCode/photoDB.sql
+++ oldDBCode/photoDB.sql
@@ -1,49 +1,51 @@
+
+
 -- Create the countries table
 CREATE TABLE countries (
+    id SERIAL PRIMARY KEY,  -- 'id' should be the primary key
     country_name TEXT UNIQUE,
-    country_code TEXT UNIQUE,
-    id SERIAL PRIMARY KEY
+    country_code TEXT UNIQUE
 );
 
 -- Create the provinces table
 CREATE TABLE provinces (
-    id SERIAL PRIMARY KEY
-    country_name TEXT NOT NULL,
-    province_name TEXT NOT NULL,
+    id SERIAL PRIMARY KEY,  -- 'id' should be the primary key
+    country_id int not NULL,  -- 'country_name' as a foreign key
+    province_name text not NULL,
     province_code TEXT NOT NULL,
-    FOREIGN KEY (country_name) REFERENCES countries(country_name)
+    FOREIGN KEY (country_id) REFERENCES countries(id)
 );
 
 -- Create the cities table
 CREATE TABLE cities (
-    province_name TEXT NOT NULL,
+    id SERIAL PRIMARY KEY,  -- 'id' should be the primary key
+    province_id int not null,  -- 'province_name' as a foreign key
     city_name TEXT NOT NULL,
-    id SERIAL PRIMARY KEY,
-    FOREIGN KEY (province_name) REFERENCES provinces(province_name)
+    FOREIGN KEY (province_id) REFERENCES provinces(id)
 );
 
 -- Create the photos table
 CREATE TABLE photos (
-    id SERIAL PRIMARY KEY,
-    city_id SERIAL NOT NULL,
+    id SERIAL PRIMARY KEY,  -- 'id' should be the primary key
+    city_id int,  -- 'city_id' references 'cities.id'
     photo_filepath TEXT UNIQUE NOT NULL,
     date DATE,
     caption TEXT,
-    FOREIGN KEY (city_id) REFERENCES cities(id)
+    FOREIGN KEY (city_id) REFERENCES cities(id)  -- Correct foreign key
 );
 
 -- Create the trips table
 CREATE TABLE trips (
-    id SERIAL PRIMARY KEY,
-    trip_name TEXT NOT NULL,
+    id SERIAL PRIMARY KEY,  -- 'id' should be the primary key
+    trip_name TEXT,
     trip_date DATE,
     trip_text TEXT
 );
 
 -- Create the trip_location table
 CREATE TABLE trip_location (
-    trip_id SERIAL NOT NULL,
-    city_id SERIAL NOT NULL,
+    trip_id int NOT NULL,  -- Use INT for referencing 'trips.id'
+    city_id int NOT NULL,  -- Use INT for referencing 'cities.id'
     PRIMARY KEY (trip_id, city_id),
     FOREIGN KEY (trip_id) REFERENCES trips(id),
     FOREIGN KEY (city_id) REFERENCES cities(id)
@@ -51,18 +53,19 @@ CREATE TABLE trip_location (
 
 -- Create the memories table
 CREATE TABLE memories (
-    memory_id SERIAL PRIMARY KEY,
-    trip_id SERIAL NOT NULL,
-    memory_text TEXT NOT NULL,
+    id SERIAL PRIMARY KEY,  -- 'memory_id' should be the primary key
+    trip_id INT NOT NULL,  -- 'trip_id' references 'trips.id'
+    memory_text TEXT,
+    memory_name TEXT
     FOREIGN KEY (trip_id) REFERENCES trips(id)
 );
 
 -- Create the memory_photo table
 CREATE TABLE memory_photo (
-    memory_id SERIAL NOT NULL,
-    photo_id SERIAL NOT NULL,
+    memory_id INT NOT NULL,  -- Use INT for referencing 'memories.memory_id'
+    photo_id INT NOT NULL,   -- Use INT for referencing 'photos.id'
     PRIMARY KEY (memory_id, photo_id),
-    FOREIGN KEY (memory_id) REFERENCES memories(memory_id),
+    FOREIGN KEY (memory_id) REFERENCES memories(id),
     FOREIGN KEY (photo_id) REFERENCES photos(id)
 );
 
